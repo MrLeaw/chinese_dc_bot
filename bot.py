01bfa784879315db78db1f8b62d4109c98b0be51
Felix Neumann <felix23.fn@gmail.com>
2022-10-04 17:33:38 +0800

    switched over to py-cord

--- bot.py
+++ bot.py
@@ -1,22 +1,14 @@
 import discord
 from discord.ext import commands
 import os
-from commands import Commands
 from dotenv import load_dotenv
 load_dotenv()
 
 BOTTOKEN = os.getenv('BOTTOKEN')
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
+bot = commands.Bot()
+@bot.slash_command(name="ping")
+async def first_slash(ctx): 
+    await ctx.respond("pong")
 
-    async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
-
-
-
-intents = discord.Intents.default()
-
-client = MyClient(intents=intents)
-client.run(BOTTOKEN)
+bot.run(BOTTOKEN)
