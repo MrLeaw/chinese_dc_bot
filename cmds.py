420cb0b69f90d964ae620a5fbf1b96899b1aeb29
Dixon Marcellino <dixonmarcellino@gmail.com>
2022-10-04 17:54:10 +0800

    echo command

--- cmds.py
+++ cmds.py
@@ -2,3 +2,7 @@ def register_commands(bot):
     @bot.slash_command(name="ping")
     async def first_slash(ctx):
         await ctx.respond("pong")
+
+    @bot.slash_command(name="echo")
+    async def second_slash(ctx, message: str):
+        await ctx.respond(message)
